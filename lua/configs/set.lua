ba5d39caba5ed9c9bf1c35e6985a52d50fb0ba68
vellone <fabriciovellone@gmail.com>
2024-09-23 18:31:32 -0400

    feat: add yank to ctrl+c

--- lua/configs/set.lua
+++ lua/configs/set.lua
@@ -27,3 +27,16 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
+
+vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+         ["+"] = "win32yank -i --crlf",
+         ["*"] = "win32yank -i --crlf"
+    },
+    paste = {
+        ["+"] = "win32yank -o --lf",
+        ["*"] = "win32yank -o --lf"
+    },
+    cache_enabled = false
+}
