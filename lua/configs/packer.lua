680aee33d2257a2db2b982a4b653a4cca092f1ca
vellone <fabriciovellone@gmail.com>
2024-09-23 18:33:39 -0400

    feat: disable copilot

--- lua/configs/packer.lua
+++ lua/configs/packer.lua
@@ -79,7 +79,7 @@ return require('packer').startup(function(use)
 
     use "lukas-reineke/indent-blankline.nvim"
 
-    use "github/copilot.vim"
+    -- use "github/copilot.vim"
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
